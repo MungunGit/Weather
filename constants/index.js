211cd9b528476d95c131c0762c0fc0917a5c49fe
Mungun <Mungunerdeneblog@gmail.com>
2023-09-09 16:07:52 +0800

    Weahter task

---
+++ constants/index.js
@@ -0,0 +1,23 @@
+// key
+export const apiKey = '03638bb509cd4c9584c83707230809';
+
+
+// weather icons and images
+export const weatherImages = {
+    'Partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Clear': require('../assets/images/sun.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/moderaterain.png'),
+    'Moderate rain at times': require('../assets/images/moderaterain.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'Mist': require('../assets/images/mist.png'),
+    'other': require('../assets/images/moderaterain.png')
+}
